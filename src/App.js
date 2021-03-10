1c8047d2061b5b3ec7e1f1ffd0982b21094e96d2
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-03-10 16:38:05 +0100

    layout hero filter section and modify global style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
 import Header from "./components/Header";
+import HeroFilter from "./components/HeroFilter";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HeroFilter />
     </div>
   );
 }
---
+++ src/components/HeroFilter/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Select from "react-select";
+import "./HeroFilterStyles.scss";
+
+const options = [
+  { value: "all ", label: "All Product" },
+  { value: "soap", label: "Soap" },
+  { value: "cream", label: "Cream" },
+];
+
+function HeroFilter() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  return (
+    <section className="hero-filter">
+      <div className="hero-filter__left">
+        <h1>All Products</h1>
+        <p>A 360° look at Lumin</p>
+      </div>
+      <div className="hero-filter__right">
+        <Select
+          className="filter"
+          value={selectedOption}
+          onChange={(selectedOption) => setSelectedOption(selectedOption)}
+          options={options}
+          isSearchable={false}
+          placeholder="Filter by"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default HeroFilter;
