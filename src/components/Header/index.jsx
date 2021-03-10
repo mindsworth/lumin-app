bcc9a6e4c3871dd0461f3479f18cf3358fbda75e
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-03-10 14:32:55 +0100

    setup header document

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">LUMIN</div>
+      <nav className="main-nav">
+        <ul className="main-nav__left">
+          <li>Shop</li>
+          <li>Learn</li>
+        </ul>
+        <ul className="main-nav__right">
+          <li>Account</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
