15d796a99e8ce4355da702189cfb129ebd7d0ce7
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-03-10 17:09:29 +0100

    product listing section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import HeroFilter from "./components/HeroFilter";
+import ProductListing from "./components/ProductListing";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroFilter />
+      <ProductListing />
     </div>
   );
 }
---
+++ src/components/ProductListing/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProductListing() {
+  return (
+    <div className="products">
+      <div className="container">here</div>
+    </div>
+  );
+}
+
+export default ProductListing;
