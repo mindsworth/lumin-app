8456252397d3c7b1db4fa0c6ca4632e75a643ac2
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-03-10 19:55:12 +0100

    add dialog header title

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -5,9 +5,12 @@ function Modal() {
   return (
     <div className="modal">
       <div className="dialog">
-          <div className="dialog__header">
-              <div></div>
+        <div className="dialog__header">
+          <div className="back-btn">
+            <i className="fa fa-angle-left" aria-hidden="true" />
           </div>
+          <div className="title">Your Cart</div>
+        </div>
       </div>
     </div>
   );
