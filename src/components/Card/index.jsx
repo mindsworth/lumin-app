2e674a1a8bf1ecff0f25f0834ba2513ec0f98506
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-03-11 05:09:13 +0100

    setup localStorage

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { update } from "idb-keyval";
 import "./CardStyling.scss";
 
 function Card({ data }) {
+  const handleOnAddToCart = (data) => {
+    update("cart", (res) => {
+      let newItem;
+
+      console.log("res :", res);
+
+      if (res) {
+        return (newItem = res.map((item) => {
+          if (item.id === data.id) {
+            item.count++;
+            return item;
+          }
+
+          return item;
+        }));
+      } else {
+        newItem = { ...data, count: 1 };
+
+        return [newItem];
+      }
+    });
+  };
+
   return (
     <div className="card">
       <figure
@@ -12,7 +36,9 @@ function Card({ data }) {
         <div className="title">{data.title}</div>
         <div className="price">From ${data.price}</div>
       </figcaption>
-      <button className="btn">Add to cart</button>
+      <button className="btn" onClick={() => handleOnAddToCart(data)}>
+        Add to cart
+      </button>
     </div>
   );
 }
